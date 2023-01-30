583757a2b881135fd785075d1903965eb73ef6de
Hakan ÖZER <hakanozer02@gmail.com>
2023-01-30 14:30:38 +0300

    1-Days

---
+++ Login/src/main/java/com/works/restcontrollers/CustomerRestController.java
@@ -0,0 +1,36 @@
+package com.works.restcontrollers;
+
+import com.works.entities.Customer;
+import com.works.services.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/customer")
+@RequiredArgsConstructor
+public class CustomerRestController {
+
+    final CustomerService customerService;
+
+    @PostMapping("/register")
+    public ResponseEntity register( @RequestBody Customer customer ) {
+        return customerService.register(customer);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login( @RequestBody Customer customer ) {
+        return customerService.login(customer);
+    }
+
+    @PostMapping("/status")
+    public ResponseEntity status(HttpServletRequest req) {
+        return customerService.status(req);
+    }
+
+}
